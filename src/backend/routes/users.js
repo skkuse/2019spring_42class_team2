c08e3a26ab51bfa76caefe2acde58570049eb249
HWhale <masking112@gmail.com>
2019-05-23 09:51:46 +0900

    fix db settings

--- src/backend/routes/users.js
+++ src/backend/routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const models = require('../models/');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  let users = await models.user.findAll();
+  console.log(users);
   res.send('respond with a resource');
+});
+
+router.post('/', function(req, res, next) {
+
 });
 
 module.exports = router;
